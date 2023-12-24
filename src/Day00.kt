d1a42ccb4e9c01b088d4d85dde4ea9a5f03a7cbd
Lee Seung Yun <ileilliat@gmail.com>
2023-12-25 01:11:11 +0900

    Add basic day template file

---
+++ src/Day00.kt
@@ -0,0 +1,17 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 1
+    }
+
+    fun part2(input: List<String>): Int {
+        return 1
+    }
+
+    val input = readInput("Day01")
+    part1(input).println()
+    part2(input).println()
+}
+
+
+
+
